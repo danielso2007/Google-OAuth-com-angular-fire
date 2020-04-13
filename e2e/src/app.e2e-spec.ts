2e0d1bea1c82005e6d7349842d363ba1bba3046b
daniel.oliveira <danielso2007@gmail.com>
2020-04-12 21:38:53 -0300

    chore: Adjust in build and deploy firebase.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('awesomeApp app is running!');
+    expect(page.getTitleText()).toEqual('google-oauth-com-angular-fire-app app is running!');
   });
 
   afterEach(async () => {
